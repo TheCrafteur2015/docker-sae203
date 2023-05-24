9d74c8f219eaed887a9c7b447dbf10302aad19b9
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2023-05-24 08:30:45 +0200

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -4,80 +4,94 @@ import java.util.*;
 
 public class ChatServer {
 	
-    public ServerSocket server;
-    public Socket client = null;
-    public DataOutputStream os;
-    public DataInputStream is;
-    public Map<String, Socket> clientList = new HashMap<>();
+    public ServerSocket        server;
+    public Socket              client;
 	
-    public static void main(String[] args){
+    public DataOutputStream    os;
+    public DataInputStream     is;
+	
+    public Map<String, Socket> clientList;
+	
+	public ChatServer() {
+		this.client = null;
+		this.clientList = new HashMap<>();
+	}	
+	
+    public static void main(String[] args) {
         ChatServer a = new ChatServer();
-        a.doConnections();
+        a.connections();
     }
 	
-    public void doConnections(){
-        try{
-        server = new ServerSocket(9001);
-        MyThreadServer messageRouterThread = new MyThreadServer();
-        messageRouterThread.start();
-        while(true)
-        {
-            
-            client = server.accept();
-            if(client != null)
-            {
-                os = new DataOutputStream(client.getOutputStream());
-                is = new DataInputStream(client.getInputStream());    
-                String requestedClientName = is.readUTF();
-                clientList.put(requestedClientName,client);
-                os.writeUTF("#accepted");
-                messageRouterThread.clientList.put(requestedClientName,client);
-            } 
-        }
-      }
-      catch(Exception e){
-        System.out.println("Error Occured!" +  e.getMessage());
-      }
-    }
+    public void connections()c{
+        try {
+			server = new ServerSocket(9001);
+			MyThreadServer messageRouterThread = new MyThreadServer();
+			messageRouterThread.start();
+			
+			while (true) {
+				client = server.accept();
+				
+				if (client != null) {
+					os = new DataOutputStream(client.getOutputStream());
+					is = new DataInputStream(client.getInputStream());
+					
+					String requestedClientName = is.readUTF();
+					clientList.put(requestedClientName, client);
+					os.writeUTF("#accepted");
+					messageRouterThread.clientList.put(requestedClientName, client);
+				} 
+			}
+		} catch(Exception e) {
+			System.out.println("Error Occured!");
+			e.printStackTrace(System.out);
+		}
+	}
 }
-class MyThreadServer extends Thread{
-    public HashMap<String,Socket> clientList = new HashMap<String,Socket>();
-    public DataInputStream is= null;
-    public DataOutputStream os=null;
-    public void run(){
+
+class MyThreadServer extends Thread {
+	
+	public Map<String, Socket> clientList;
+	
+    public DataInputStream     is;
+    public DataOutputStream    os;
+	
+	public MyThreadServer() {
+		this.clientList = new HashMap<>();
+		this.is = null;
+		this.os = null;
+	}
+	
+    public void run() {
         String msg = "";
         int i = 0;
-        System.out.println("Chat Server Running .....");
+		
+        System.out.println("Serveur de conversation en cours d'exécution .....");
         String toClientName = "";
-        while(true){
-            try{
-                if(clientList != null){
-                    for(String key: clientList.keySet())
-                    {
-                        is= new DataInputStream(clientList.get(key).getInputStream());
-                        if(is.available()>0)
-                        {
-                            msg=is.readUTF();
-                            i= msg.indexOf("@");
-                            toClientName = msg.substring(i);
-                            if(clientList.containsKey(toClientName)){
-                                os= new DataOutputStream(clientList.get(toClientName).getOutputStream());
-                                os.writeUTF( key + ": " + msg.substring(0,i));
-				System.out.println("@SERVER : " + key + ": " + msg.substring(0, i) + " to " + toClientName);
-                            }
-                            else{
-                                os= new DataOutputStream(clientList.get(key).getOutputStream());
-                                os.writeUTF("Message From Server : No such Username found");
-                            }
-                        }
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                System.out.println("Server Recovering Initialization Failure ...");
-                System.out.println("Server Up And Running ...");
-            }
-        }
-    }
+		
+		while (true) {
+			try {
+				if (clientList != null) {
+					for (String key: clientList.keySet()) {
+						is = new DataInputStream(clientList.get(key).getInputStream());
+						if (is.available() > 0) {
+							msg = is.readUTF();
+							i = msg.indexOf("@");
+							toClientName = msg.substring(i);
+							if (clientList.containsKey(toClientName)) {
+								os = new DataOutputStream(clientList.get(toClientName).getOutputStream());
+								os.writeUTF(key + ": " + msg.substring(0,i));
+								System.out.println("@SERVER : " + key + ": " + msg.substring(0, i) + " to " + toClientName);
+							} else {
+								os = new DataOutputStream(clientList.get(key).getOutputStream());
+								os.writeUTF("Message du serveur : aucun nom d'utilisateur trouvé");
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				//System.out.println("Server Recovering Initialization Failure ...");
+				System.out.println("Serveur opérationnel ...");
+			}
+		}
+	}
 }
